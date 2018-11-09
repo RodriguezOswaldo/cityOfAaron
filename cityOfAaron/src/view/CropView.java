6982abbb7e75c763ed24153902a94f075f5f6705
RodriguezOswaldo <ownsan@byui.edu>
2018-11-08 23:20:52 -0500

    9/24/2918 - Oswaldo CropView Class

---
+++ cityOfAaron/src/view/CropView.java
@@ -0,0 +1,142 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+import model.*;
+import control.*;
+import java.util.Scanner;
+import cityofaaron.CityOfAaron;
+
+/**
+ *
+ * @author Own
+ */
+public class CropView {
+    
+    //Create a Scanner object
+    private static Scanner keyboard = new Scanner(System.in);
+    
+    //Get references to the Game object and the CropData object
+    static private Game game = CityOfAaron.getGame();
+    static private CropData cropData = game.getCropData();
+    
+    
+    //The buyLandView method
+    //Purpose: Interface with the user input for buying land
+    //Parameters: none
+    //Returns: none
+    public static void buyLandView(){
+        
+        //Get the cost of Land for this round
+        int price = CropControl.calcLandCost();
+        
+        //Prompt the user to enter the number of acres to buy
+        System.out.format("Land is selling for %d bushles per acre.%n", price);
+        System.out.println("\nHow many acres of land do you wish to buy?");
+        
+        //Get the user's input and save it.
+        int toBuy;
+        toBuy = keyboard.nextInt();
+        
+        //Call the buyLand() method in the control later to buy the land
+        CropControl.buyLand(price, toBuy, cropData);
+        
+        //Output how much land we now own
+        System.out.format("You now own %d acres of land, ", cropData.getAcresOwned());
+    }
+     //The sellLandView method
+    //Purpose: Interface with the user input for selling land
+    //Parameters: none
+    //Returns: none
+    //Created By Oswaldo Rodríguez
+     public static void sellLandView(){
+        
+        //Get the cost of Land for this round
+        int price = CropControl.calcLandCost();
+        
+        //Prompt the user to enter the number of acres to sell
+        System.out.format("Land is selling for %d bushles per acre.%n", price);
+        System.out.println("\nHow many acres of land do you wish to sell?");
+        
+        //Get the user's input and save it.
+        int toSell;
+        toSell = keyboard.nextInt();
+        
+        //Call the sellLand() method in the control later to buy the land
+        CropControl.sellLand(price, toSell, cropData);
+        
+        //Output how much land we now own
+        System.out.format("You now own %d acres of land, ", cropData.getAcresOwned());
+    }
+     
+     
+        //The plantCropsView method
+        //Purpose: Interface with the user input for plant crops
+        //Parameters: none
+        //Returns: none
+        //Created 
+      public static void plantCropsView(){
+        
+        //Get the cost of Land for this round
+        int population = CropControl.calcLandCost();
+        
+        //Prompt the user to enter the number of acres to plant
+        System.out.format("Land is selling for %d bushles per acre.%n", population);//Fix this comments
+        System.out.println("\nHow many acres of land do you want to plant?”");
+        
+        //Get the user's input and save it.
+        int toPlant;
+        toPlant = keyboard.nextInt();
+       
+      
+        //Call the buyLand() method in the control later to plant the crops
+        CropControl.plantTheCrops(toPlant, cropData);
+        
+        //Output how many acres were planted
+        System.out.format("You have planted %d acres of land, ", cropData.getAcresPlanted());
+    }
+        //The feedPeopleView method
+        //Purpose: Interface with the user input for setting aside wheat to feed the people 
+        //Parameters: none
+        //Returns: none
+        //Created
+       public static void feedPeopleView(){
+        
+        //Get the cost of Land for this round
+        int population = cropData.getPopulation();
+        
+        //Prompt the user to enter the number of acres to buy
+        System.out.format("Land is selling for %d bushles per acre.%n", population);
+        System.out.println("\nHow many bushels of grain do you want to set aside to feed the people?”");
+        
+        //Get the user's input and save it.
+        int bushelsGrain;
+        bushelsGrain = keyboard.nextInt();
+        
+        cropData.setWheatForPeople(bushelsGrain);
+   
+        //Call the buyLand() method in the control later to buy the land
+        CropControl.feedPeople(cropData, bushelsGrain);
+        
+        //Output how many acres were planted
+        System.out.format("You have planted %d acres of land, ",cropData.getWheatInStore());
+    }
+    //The runCropView method()
+    //Purpose: runs the methods to manage the crops game
+    //Parameters: none
+    //Returns: none
+    public static void runCropView(){
+        
+        //call the buyLandView() method
+        buyLandView();
+        //call the sellLandView() method
+        sellLandView();
+         //call the plantCropsView() method
+        plantCropsView();
+        //call the feedPeopleView() method
+        feedPeopleView();
+        
+    }
+}
